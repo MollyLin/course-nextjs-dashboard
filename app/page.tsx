392dd52f2d1a197d0705f78ad59b1ec5a2b6e313
Molly Lin <mollylin2014@gmail.com>
2024-05-02 14:07:36 +0800

    revert

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,55 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
-export default function Page() {
+type DateTime = {
+  datetime: string,
+};
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  // else url: https://api.github.com/repos/vercel/next.js
+  const fetchVercelData = await fetch('https://api.github.com/users/vercel');
+  const vercelResData = await fetchVercelData.json();
+  const fetchChicagoDatetimeData = await fetch('https://worldtimeapi.org/api/timezone/America/Chicago', { cache: 'no-store' });
+  const chicagoResData: DateTime  = await fetchChicagoDatetimeData.json();
+
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+      <h1 className={`mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <h4 className={`text-lg font-bold`}>Data Fetch Demo:</h4>
+      <ul className={`list-inside list-disc mb-5 leading-loose border pl-5`}>
+        <li>Fake user repos url: {vercelResData.repos_url}</li>
+        <li>Fake user email: {vercelResData.email}</li>
+        <li>Fake user location: {vercelResData.location}</li>
+      </ul>
+      <p className={`mb-5`}>芝加哥現在時間: <time className={`text-sky-600`}>{chicagoResData.datetime}</time></p>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
